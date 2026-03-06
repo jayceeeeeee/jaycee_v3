a31114d3699c1824884032b7d42621a9dae932b8
cedricjaub <39049143+jayceeeeeee@users.noreply.github.com>
2026-03-06 09:54:30 +0900

    src commit -m "Move files to src directory"

--- src/script.js
+++ src/script.js
@@ -1,36 +1,43 @@
+//user input, latitude and longitude
+let humanX = 0;
+let humanY = 0;
+let humanLat = 0;
+let humanLon = 0;
 
-let lat = 0;
-let lon = 0;
+//measures of the universe
+let pi = 3.142857;
+let earthRadius = 4536; // in miles
 
+//creating the canvas and getting the geolocation of the user
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
   angleMode(RADIANS);
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
-      lat = position.coords.latitude;
-      lon = position.coords.longitude;
+      humanLat = position.coords.latitude;
+      humanLon = position.coords.longitude;
     });
   }
 }
 
+//starting to draw
 function draw() {
+  //initiating the canvas
   background(0);
   translate(width/2, height/2);
 
   stroke(255);
   noFill();
 
-  let earthRadius = 250;
-
   // Terre
   circle(0, 0, earthRadius * 2);
 
   // Conversion longitude → angle
-  let angle = map(lon, -180, 180, -PI, PI);
+  let angle = map(humanLon, -180, 180, -PI, PI);
 
   // Conversion latitude → distance
-  let distance = map(lat, -90, 90, earthRadius, 0);
+  let distance = map(humanLat, -90, 90, earthRadius, 0);
 
   let x = cos(angle) * distance;
   let y = sin(angle) * distance;
